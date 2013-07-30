e21a66673290faffbba4aac5476f7530cfd8b68d
Karan Sharma <javakaran@live.com>
2013-07-30 10:23:34 +0530

    Trinagle Class
    Triangle Class

---
+++ src/triangle/Triangle.java
@@ -0,0 +1,27 @@
+package triangle;
+
+import java.awt.Point;
+
+
+/**
+ *
+ * @author  Karan Sharma
+ * @time     Jul 29, 2013 7:39:20 AM 
+ */
+public class Triangle {
+
+    Point p1 = new Point();
+    
+/*    ...
+ */
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
